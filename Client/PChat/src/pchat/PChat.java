b27703e4d9c93dde44e70d9143d063cfd26c4581
Dixanta Bahadur Shrestha <dixanta@gmail.com>
2014-06-28 13:13:35 +0545

    Client Added

---
+++ Client/PChat/src/pchat/PChat.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pchat;
+
+/**
+ *
+ * @author forsell
+ */
+public class PChat {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
